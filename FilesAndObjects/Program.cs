87b3edcd7c1b64cf308883ccdeaa14f0d3670ecc
unknown <mammutipoja1@gmail.com>
2020-12-09 20:40:03 +0200

    FilesAndObjects

---
+++ FilesAndObjects/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FilesAndObjects
+{
+    class Program
+    {
+        class Movie
+        {
+            public string title;
+            public string rating;
+            public string year;
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\mammu\samples";
+            string filename = @"imdb.txt";
+            List<string> movieList = File.ReadAllLines(Path.Combine(filepath, filename)).ToList();
+            List<Movie> listOfMovies = new List<Movie>();
+
+
+
+            foreach (string movieItem in movieList)
+            {
+                string[] tempArray = movieItem.Split(new char[] {';'},StringSplitOptions.RemoveEmptyEntries);
+                Movie newMovie = new Movie(tempArray[0],tempArray[1],tempArray[2]);
+
+                listOfMovies.Add(newMovie);
+            }
+
+            foreach (Movie movie in listOfMovies)
+            {
+                Console.WriteLine($"Title: {movie.title}; Rating: {movie.rating}; Year: {movie.year}");
+            }
+
+            Console.WriteLine("What`s your favourite movie? enter title:");
+            string favMovieTitle = Console.ReadLine();
+            Console.WriteLine("Enter the rating of your favourite movie:");
+            string favMovieRating = Console.ReadLine();
+            Console.WriteLine("Enter release year");
+            string favMovieYear = Console.ReadLine();
+            
+            Movie favMovie = new Movie(favMovieTitle, favMovieRating, favMovieYear);
+            string favMovieLine = $"{favMovie.title};{favMovie.rating};{favMovie.year}";
+
+            movieList.Add(favMovieLine);
+            File.WriteAllLines(Path.Combine(filepath, filename), movieList);
+        }
+    }
+}
---
+++ ReadTotalFromShoppinigList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadTotalFromShoppinigList
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int price;
+            public Item(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+                Console.WriteLine($"Item {name} created");
+            }
+            
+            public string Name { get { return name; } }
+            public int Price { get { return price; } }
+
+
+        }
+        static void Main(string[] args)
+        {
+            ReadFromShoppingList();
+        }
+
+        public static void ReadFromShoppingList()
+        {
+            string filePath = @"C:\Users\mammu\samples";
+            string fileName = @"shoppingList.txt";
+            
+            List<Item> shoppingItems = new List<Item>();
+
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(Path.Combine(filePath, fileName))).ToList();
+
+            foreach (string line in linesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);                
+                Item newItem = new Item(tempArray[0], Int32.Parse(tempArray[1]));
+                shoppingItems.Add(newItem);
+            }
+
+            Console.WriteLine("Your shopping cart: ");
+            int total = 0;
+
+            foreach (Item item in shoppingItems)
+            {
+                Console.WriteLine($"{item.Name}; Price: {item.Price}");
+                total += item.Price;
+            }
+            Console.WriteLine($"Your shopping cart total: {total} credits ");
+
+        }
+
+
+
+    }
+}
